3487212f380c83e57743ad932554183c30561063
Asasing37 <samuelcuidaesquivel@gmail.com>
2024-09-13 15:41:52 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>Php</title></head>
+    <meta http-equiv="conet-type" content="text/html; charset=UTF-8"/>
+    <meta charsed = "UTF-8">
+    <body>
+        <front size = "5" color="red" face="comic sans ms">
+            <center>Bienvenido Al Mundo Php</center>
+        </font>
+        <?php
+        echo "hola mundo en php <p>";
+        ?>
+        <body>
+</html>
